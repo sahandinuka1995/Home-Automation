8dc1ea3b1330ebba3426bdd8189e7b161745f8cf
sahandinuka1995 <sahandinuka1995@gmail.com>
2020-09-02 17:19:28 +0530

    first

---
+++ MyHomeAutomation/MyHomeAutomation/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace MyHomeAutomation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.bunifuDragControl1 = new ns1.BunifuDragControl(this.components);
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuiOSSwitch2 = new ns1.BunifuiOSSwitch();
+            this.bunifuiOSSwitch1 = new ns1.BunifuiOSSwitch();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(0, 12, 0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "MyHome Automation System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Light";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Fan";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.LightGray;
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(265, 42);
+            this.flowLayoutPanel1.TabIndex = 10;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.flowLayoutPanel1;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::MyHomeAutomation.Properties.Resources.lightOff;
+            this.pictureBox5.Location = new System.Drawing.Point(171, 86);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::MyHomeAutomation.Properties.Resources.fanOff;
+            this.pictureBox4.Location = new System.Drawing.Point(171, 129);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MyHomeAutomation.Properties.Resources.lightOn;
+            this.pictureBox3.Location = new System.Drawing.Point(171, 86);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MyHomeAutomation.Properties.Resources.fanOn;
+            this.pictureBox2.Location = new System.Drawing.Point(171, 129);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MyHomeAutomation.Properties.Resources.Home_96px;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(20, 6, 5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bunifuiOSSwitch2
+            // 
+            this.bunifuiOSSwitch2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuiOSSwitch2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuiOSSwitch2.BackgroundImage")));
+            this.bunifuiOSSwitch2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuiOSSwitch2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuiOSSwitch2.Location = new System.Drawing.Point(103, 130);
+            this.bunifuiOSSwitch2.Name = "bunifuiOSSwitch2";
+            this.bunifuiOSSwitch2.OffColor = System.Drawing.Color.Gray;
+            this.bunifuiOSSwitch2.OnColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(202)))), ((int)(((byte)(94)))));
+            this.bunifuiOSSwitch2.Size = new System.Drawing.Size(43, 25);
+            this.bunifuiOSSwitch2.TabIndex = 7;
+            this.bunifuiOSSwitch2.Value = false;
+            this.bunifuiOSSwitch2.OnValueChange += new System.EventHandler(this.bunifuiOSSwitch2_OnValueChange);
+            // 
+            // bunifuiOSSwitch1
+            // 
+            this.bunifuiOSSwitch1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuiOSSwitch1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuiOSSwitch1.BackgroundImage")));
+            this.bunifuiOSSwitch1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuiOSSwitch1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuiOSSwitch1.Location = new System.Drawing.Point(103, 88);
+            this.bunifuiOSSwitch1.Name = "bunifuiOSSwitch1";
+            this.bunifuiOSSwitch1.OffColor = System.Drawing.Color.Gray;
+            this.bunifuiOSSwitch1.OnColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(202)))), ((int)(((byte)(94)))));
+            this.bunifuiOSSwitch1.Size = new System.Drawing.Size(43, 25);
+            this.bunifuiOSSwitch1.TabIndex = 5;
+            this.bunifuiOSSwitch1.Value = false;
+            this.bunifuiOSSwitch1.OnValueChange += new System.EventHandler(this.bunifuiOSSwitch1_OnValueChange);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Status : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(127, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Offline";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(265, 177);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bunifuiOSSwitch2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bunifuiOSSwitch1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MyHome Automation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private ns1.BunifuiOSSwitch bunifuiOSSwitch1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private ns1.BunifuiOSSwitch bunifuiOSSwitch2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private ns1.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MyHomeAutomation/MyHomeAutomation/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHomeAutomation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuiOSSwitch1_OnValueChange(object sender, EventArgs e)
+        {
+            if (checkInternet())
+            {
+                if (bunifuiOSSwitch1.Value == true)
+            {
+                lightOn();
+                pictureBox3.Show();
+                pictureBox5.Hide();
+            }
+            else {
+                lightOff();
+                pictureBox3.Hide();
+                pictureBox5.Show();
+            }
+            }
+        }
+
+        private void lightOn() {
+            string url = "https://188.166.206.43/20MOrdfADZ3yHxb-tyDRzxdndaZ8j2XT/update/D16?value=1";
+            WebResponse response = null;
+            string data = string.Empty;
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new
+                RemoteCertificateValidationCallback
+                (
+                    delegate { return true; }
+                );
+
+                WebRequest request = WebRequest.Create(url);
+                response = request.GetResponse();
+            }
+            catch (WebException ee)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+            finally
+            {
+                if (response != null) response.Close();
+            }
+        }
+
+        private void lightOff()
+        {
+            string url = "https://188.166.206.43/20MOrdfADZ3yHxb-tyDRzxdndaZ8j2XT/update/D16?value=0";
+            WebResponse response = null;
+            string data = string.Empty;
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new
+                RemoteCertificateValidationCallback
+                (
+                    delegate { return true; }
+                );
+
+                WebRequest request = WebRequest.Create(url);
+                response = request.GetResponse();
+            }
+            catch (WebException ee)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+            finally
+            {
+                if (response != null) response.Close();
+            }
+        }
+
+        private void bunifuiOSSwitch2_OnValueChange(object sender, EventArgs e)
+        {
+            if (checkInternet())
+            {
+                if (bunifuiOSSwitch2.Value == true)
+            {
+                fanOn();
+                pictureBox2.Show();
+                pictureBox4.Hide();
+            }
+            else
+            {
+                fanOff();
+                pictureBox2.Hide();
+                pictureBox4.Show();
+            }
+            }
+        }
+
+        private void fanOn()
+        {
+            string url = "https://188.166.206.43/20MOrdfADZ3yHxb-tyDRzxdndaZ8j2XT/update/D0?value=1";
+            WebResponse response = null;
+            string data = string.Empty;
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new
+                RemoteCertificateValidationCallback
+                (
+                    delegate { return true; }
+                );
+
+                WebRequest request = WebRequest.Create(url);
+                response = request.GetResponse();
+            }
+            catch (WebException ee)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+            finally
+            {
+                if (response != null) response.Close();
+            }
+        }
+
+        private void fanOff()
+        {
+            string url = "https://188.166.206.43/20MOrdfADZ3yHxb-tyDRzxdndaZ8j2XT/update/D0?value=0";
+            WebResponse response = null;
+            string data = string.Empty;
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new
+                RemoteCertificateValidationCallback
+                (
+                    delegate { return true; }
+                );
+
+                WebRequest request = WebRequest.Create(url);
+                response = request.GetResponse();
+            }
+            catch (WebException ee)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("No Internet Connection");
+            }
+            finally
+            {
+                if (response != null) response.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkInternet();
+        }
+
+        private Boolean checkInternet()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com"))
+
+                label5.ForeColor = Color.Green;
+                label5.Text = "Online";
+                return true;
+            }
+            catch
+            {
+                label5.ForeColor = Color.Red;
+                label5.Text = "Offline";
+
+                pictureBox2.Hide();
+                pictureBox4.Show();
+
+                pictureBox3.Hide();
+                pictureBox5.Show();
+
+                //bunifuiOSSwitch1.Value = false;
+                //bunifuiOSSwitch2.Value = false;
+            }
+            return false;
+        }
+    }
+}
